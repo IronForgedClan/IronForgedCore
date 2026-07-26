56474632b598134971b215e0778918af5649c7fb
Barnz <hi@barnz.dev>
2026-07-26 10:22:47 +0100

    fix incorrect down revision id

--- ironforgedcore/alembic/versions/e1a2b3c4d5e6_expand_api_audit_columns.py
+++ ironforgedcore/alembic/versions/e1a2b3c4d5e6_expand_api_audit_columns.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "e1a2b3c4d5e6"
-down_revision: Union[str, None] = "12c509b3fd8c"
+down_revision: Union[str, None] = "c3d4e5f6a7b8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
